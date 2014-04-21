234eba9509873c5ff54efb8d797d26ae9ce265df
dharris5009 <dharris5009@bvsd.org>
2014-04-21 12:17:16 -0600

    Added Buttons

--- CountDown/CountDown/ViewController.h
+++ CountDown/CountDown/ViewController.h
@@ -9,5 +9,15 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+- (IBAction)minus:(UIButton *)sender;
+- (IBAction)plus:(UIButton *)sender;
+- (IBAction)minus10:(UIButton *)sender;
+- (IBAction)plus10:(UIButton *)sender;
+- (IBAction)minus100:(UIButton *)sender;
+- (IBAction)plus100:(UIButton *)sender;
+- (IBAction)minus1000:(UIButton *)sender;
+- (IBAction)plus1000:(UIButton *)sender;
+@property (weak, nonatomic) IBOutlet UILabel *counter;
+
 
 @end
--- CountDown/CountDown/ViewController.m
+++ CountDown/CountDown/ViewController.m
@@ -14,16 +14,52 @@
 
 @implementation ViewController
 
+@synthesize counter;
+
+int count = 0;
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    counter.text = [NSString stringWithFormat:@"%i", count];
+    self.view.backgroundColor = [UIColor blackColor];
+}
+- (IBAction)minus:(id)sender {
+    count = count - 1;
+    counter.text = [NSString stringWithFormat:@"%i", count];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)plus:(id)sender {
+    count = count + 1;
+    counter.text = [NSString stringWithFormat:@"%i",  count];
 }
 
-@end
+- (IBAction)plus10:(id)sender {
+    count = count + 10;
+    counter.text = [NSString stringWithFormat:@"%i",  count];
+}
+
+- (IBAction)minus10:(id)sender {
+    count = count - 10;
+    counter.text = [NSString stringWithFormat:@"%i",  count];
+}
+
+- (IBAction)minus100:(id)sender {
+    count = count - 100;
+    counter.text = [NSString stringWithFormat:@"%i", count];
+}
+
+- (IBAction)plus100:(id)sender {
+    count = count + 100;
+    counter.text = [NSString stringWithFormat:@"%i",  count];
+}
+
+- (IBAction)minus1000:(id)sender {
+    count = count - 1000;
+    counter.text = [NSString stringWithFormat:@"%i", count];
+}
+- (IBAction)plus1000:(id)sender {
+        count = count - 1000;
+        counter.text = [NSString stringWithFormat:@"%i", count];
+}
+ @end
