f07f0289d043b91e72ea6316fd468edcab16260b
kinghenryharris <kinghenryharris@gmail.com>
2014-04-21 13:12:02 -0600

    restarted

---
+++ CountDown/CountDown/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CountDown
+//
+//  Created by Henry Harris on 4/21/14.
+//  Copyright (c) 2014 Dummy Code. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CountDown/CountDown/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CountDown
+//
+//  Created by Henry Harris on 4/21/14.
+//  Copyright (c) 2014 Dummy Code. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
